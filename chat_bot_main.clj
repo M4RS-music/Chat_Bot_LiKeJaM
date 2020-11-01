014f696cd1683657fef9869bdf6fbcc76e8cf591
mars-janda <martin.janda@praguecollege.cz>
2020-11-01 11:20:53 +0100

    Input Bertramka info
    This commit should serve as the template for all parks data, however i believe only one park is necesary for the ICA1. The hashmap parks_info will eventually contain sub-hashmaps for every park on the website. All the info is structured in a way that the user can ask numerous questions where available such as.
    Can I take a (tram, bus, metro) there?
    What (tram, bus, metro) can I take?
    What type of parking is there?
    Where is the parking?
    What are the opening hourse in (month)?
    
    etc.
    
    Commas in this map have been ommited and replaced by newlines. I think this looks much better but it does not have to stay that way.
    
    --MARTIN november 1 10:30 - 11:20 am

--- chat_bot_main.clj
+++ chat_bot_main.clj
@@ -1,3 +1,28 @@
+(def parks_info
+  {
+    :Bertramka {
+      :WC true
+      :Bike true
+      :Rolerblades false
+      :Sport_Ground false
+      :Playground false
+      :MHD {
+              :Tram [4, 7, 9, 10, 58, 59]
+              :Bus nil
+              :Metro nil}
+      :GPS "50°4’15.00’’ N, 14°23’42.00’’ E"
+      :Parking {
+                :Type "undeground"
+                :Where "OC Smichov"}
+      :Trail_Type "cobblestone"
+      :Owner "Městská část Praha 5 a spol. Comenius"
+      :Hours { ;;User will ask "When open", machine ask "what month" then machine gives either on or off hrs
+              :On_Season ["April", "May", "June", "July", "August", "September", "October"]
+              :Off_Season ["November", "December", "January", "February", "March"]
+              :On_Hours "9-18"
+              :Off_Hours "9:30-16"}
+      :Website "https://www.praha.eu/jnp/cz/co_delat_v_praze/parky/bertramka/index.html"}})
+
 (defn process_input [input]
   (println "Machine says:" input))
 
